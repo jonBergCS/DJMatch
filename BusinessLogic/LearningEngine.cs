c6e910b10b92138bee2767c09a8e5d4e50919803
jonBergCS <johnathanberg@cs.colman.ac.il>
2018-01-04 20:13:07 +0200

    kaki

--- BusinessLogic/LearningEngine.cs
+++ BusinessLogic/LearningEngine.cs
@@ -1,4 +1,6 @@
-﻿using Accord.Neuro;
+﻿using Accord.MachineLearning.DecisionTrees;
+using Accord.Neuro;
+using DJMatch.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,9 +11,22 @@ namespace BusinessLogic
 {
     public class LearningEngine
     {
+        private DecisionTree dtree;
+        private DJMatchEntities db = new DJMatchEntities();
+
         public LearningEngine()
         {
-            
-        }  
+            this.Learn();
+        }
+
+        private void Learn()
+        {
+            List<DecisionVariable> dvar = new List<DecisionVariable>();
+            dvar.Add(new DecisionVariable("Budget", 4));
+            dvar.Add(new DecisionVariable("Genre", DecisionVariableKind.Discrete));
+            dvar.Add(new DecisionVariable("Area", 4));
+
+            this.dtree = new DecisionTree(dvar, 100);
+        }
     }
 }
