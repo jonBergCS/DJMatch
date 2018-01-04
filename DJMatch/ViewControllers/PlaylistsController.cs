fdf8fd9fa9b7e8b0d081d92872a8b45a79463ccf
jonBergCS <johnathanberg@cs.colman.ac.il>
2018-01-04 18:58:08 +0200

    Oh god almighty

---
+++ DJMatch/ViewControllers/PlaylistsController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using DJMatch.Models;
+
+namespace DJMatch.ViewControllers
+{
+    public class PlaylistsController : Controller
+    {
+        private DJMatchEntities db = new DJMatchEntities();
+
+        // GET: Playlists
+        public ActionResult Index()
+        {
+            var playlists = db.Playlists.Include(p => p.DJ).Include(p => p.User);
+            return View(playlists.ToList());
+        }
+
+        // GET: Playlists/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Playlist playlist = db.Playlists.Find(id);
+            if (playlist == null)
+            {
+                return HttpNotFound();
+            }
+            return View(playlist);
+        }
+
+        // GET: Playlists/Create
+        public ActionResult Create()
+        {
+            ViewBag.DJ_ID = new SelectList(db.DJs, "ID", "Name");
+            ViewBag.UserID = new SelectList(db.Users, "ID", "Name");
+            return View();
+        }
+
+        // POST: Playlists/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,DJ_ID,UserID,Name,Picture")] Playlist playlist)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Playlists.Add(playlist);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.DJ_ID = new SelectList(db.DJs, "ID", "Name", playlist.DJ_ID);
+            ViewBag.UserID = new SelectList(db.Users, "ID", "Name", playlist.UserID);
+            return View(playlist);
+        }
+
+        // GET: Playlists/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Playlist playlist = db.Playlists.Find(id);
+            if (playlist == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.DJ_ID = new SelectList(db.DJs, "ID", "Name", playlist.DJ_ID);
+            ViewBag.UserID = new SelectList(db.Users, "ID", "Name", playlist.UserID);
+            return View(playlist);
+        }
+
+        // POST: Playlists/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,DJ_ID,UserID,Name,Picture")] Playlist playlist)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(playlist).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.DJ_ID = new SelectList(db.DJs, "ID", "Name", playlist.DJ_ID);
+            ViewBag.UserID = new SelectList(db.Users, "ID", "Name", playlist.UserID);
+            return View(playlist);
+        }
+
+        // GET: Playlists/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Playlist playlist = db.Playlists.Find(id);
+            if (playlist == null)
+            {
+                return HttpNotFound();
+            }
+            return View(playlist);
+        }
+
+        // POST: Playlists/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Playlist playlist = db.Playlists.Find(id);
+            db.Playlists.Remove(playlist);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ DJMatch/ViewControllers/ReviewsController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using DJMatch.Models;
+
+namespace DJMatch.ViewControllers
+{
+    public class ReviewsController : Controller
+    {
+        private DJMatchEntities db = new DJMatchEntities();
+
+        // GET: Reviews
+        public ActionResult Index()
+        {
+            var reviews = db.Reviews.Include(r => r.DJ).Include(r => r.User);
+            return View(reviews.ToList());
+        }
+
+        // GET: Reviews/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Review review = db.Reviews.Find(id);
+            if (review == null)
+            {
+                return HttpNotFound();
+            }
+            return View(review);
+        }
+
+        // GET: Reviews/Create
+        public ActionResult Create()
+        {
+            ViewBag.DJ_ID = new SelectList(db.DJs, "ID", "Name");
+            ViewBag.UserID = new SelectList(db.Users, "ID", "Name");
+            return View();
+        }
+
+        // POST: Reviews/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,DJ_ID,UserID,Text,Score,Date")] Review review)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Reviews.Add(review);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.DJ_ID = new SelectList(db.DJs, "ID", "Name", review.DJ_ID);
+            ViewBag.UserID = new SelectList(db.Users, "ID", "Name", review.UserID);
+            return View(review);
+        }
+
+        // GET: Reviews/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Review review = db.Reviews.Find(id);
+            if (review == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.DJ_ID = new SelectList(db.DJs, "ID", "Name", review.DJ_ID);
+            ViewBag.UserID = new SelectList(db.Users, "ID", "Name", review.UserID);
+            return View(review);
+        }
+
+        // POST: Reviews/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,DJ_ID,UserID,Text,Score,Date")] Review review)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(review).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.DJ_ID = new SelectList(db.DJs, "ID", "Name", review.DJ_ID);
+            ViewBag.UserID = new SelectList(db.Users, "ID", "Name", review.UserID);
+            return View(review);
+        }
+
+        // GET: Reviews/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Review review = db.Reviews.Find(id);
+            if (review == null)
+            {
+                return HttpNotFound();
+            }
+            return View(review);
+        }
+
+        // POST: Reviews/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Review review = db.Reviews.Find(id);
+            db.Reviews.Remove(review);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
