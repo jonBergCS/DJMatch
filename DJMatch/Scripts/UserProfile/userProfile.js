e7b5eb5e9cb6e72447af308368cfaee48349d166
שיר זרחיה < @LAPTOP-CTMBKJS4>
2018-01-08 00:48:55 +0200

    הוספת מעבר בין שאלות

--- DJMatch/Scripts/UserProfile/userProfile.js
+++ DJMatch/Scripts/UserProfile/userProfile.js
@@ -4,5 +4,10 @@
         url: url + '/Questions'
     }).then(function successCallback(response) {
         $scope.questions = response.data;
-    });
+        $scope.currentIndex = 1;
+        });
+
+    $scope.next = function () {
+        $scope.currentIndex++;
+    };
 });
