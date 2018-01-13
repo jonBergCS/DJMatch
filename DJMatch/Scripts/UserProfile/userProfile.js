f69d8e2cb9c72595d0ac82dd59dce26801cbc7e9
שיר זרחיה < @LAPTOP-CTMBKJS4>
2018-01-14 00:32:36 +0200

    תיקון אחרי שינוי צד שרת

--- DJMatch/Scripts/UserProfile/userProfile.js
+++ DJMatch/Scripts/UserProfile/userProfile.js
@@ -4,15 +4,31 @@
         url: url + '/api/Questions'
     }).then(function successCallback(response) {
         $scope.questions = response.data;
-        $scope.currentIndex = 1;
-        $scope.userAnswers = {};
+        $http({
+            method: 'GET',
+            url: url + '/api/Answers'
+        }).then(function successCallback(response) {
+            $scope.answers = response.data;
+            $scope.currentIndex = 1;
+            $scope.userAnswers = {};
 
-        // TODO : change according to previous answers
-        for (var i = 0; i < $scope.questions.length; i++) {
-            for (var j = 0; j < $scope.questions[i].Answers.length; j++) {
-                $scope.userAnswers[$scope.questions[i].Answers[j].ID] = false;
+            // TODO : change according to previous answers
+            //for (var i = 0; i < $scope.questions.length; i++) {
+            //    for (var j = 0; j < $scope.questions[i].Answers.length; j++) {
+            //        $scope.userAnswers[$scope.questions[i].Answers[j].ID] = false;
+            //    }
+            //}
+
+            for (var i = 0; i < $scope.questions.length; i++) {
+                $scope.questions[i].Answers = [];
             }
-        }
+
+            for (var i = 0; i < $scope.answers.length; i++) {
+                $scope.userAnswers[$scope.answers[i].ID] = false;
+                $scope.questions[$scope.answers[i].QuestionID - 1].Answers.
+                    push({ ID: $scope.answers[i].ID, Text: $scope.answers[i].Text });
+            }
+        });
     });
 
     $scope.save = function (user, question, answer) {
