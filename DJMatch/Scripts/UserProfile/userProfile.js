ec7e28a0b8a121187e7139b991844ce29ccf94d9
שיר זרחיה < @LAPTOP-CTMBKJS4>
2018-01-20 14:36:07 +0200

    הוספת שליפה של התשובות הקודמות

--- DJMatch/Scripts/UserProfile/userProfile.js
+++ DJMatch/Scripts/UserProfile/userProfile.js
@@ -4,34 +4,37 @@
         url: url + '/api/Questions'
     }).then(function successCallback(response) {
         $scope.questions = response.data;
+
         $http({
             method: 'GET',
             url: url + '/api/Answers'
         }).then(function successCallback(response) {
             $scope.answers = response.data;
-            $scope.currentIndex = 1;
-            $scope.userAnswers = {};
 
-            // TODO : change according to previous answers
-            //for (var i = 0; i < $scope.questions.length; i++) {
-            //    for (var j = 0; j < $scope.questions[i].Answers.length; j++) {
-            //        $scope.userAnswers[$scope.questions[i].Answers[j].ID] = false;
-            //    }
-            //}
+            $http({
+                method: 'GET',
+                //TODO: change the user id
+                url: url + '/api/UserAnswers/1'
+            }).then(function successCallback(response) {
+                a = response.data; 
 
-            for (var i = 0; i < $scope.questions.length; i++) {
-                $scope.questions[i].Answers = [];
-            }
+                $scope.currentIndex = 1;
+                $scope.userAnswers = {};
 
-            for (var i = 0; i < $scope.answers.length; i++) {
-                $scope.userAnswers[$scope.answers[i].ID] = false;
-                $scope.questions[$scope.answers[i].QuestionID - 1].Answers.
-                    push({ ID: $scope.answers[i].ID, Text: $scope.answers[i].Text });
-            }
+                for (var i = 0; i < $scope.questions.length; i++) {
+                    $scope.questions[i].Answers = [];
+                }
+
+                for (var i = 0; i < $scope.answers.length; i++) {
+                    $scope.userAnswers[$scope.answers[i].ID] = false;
+                    $scope.questions[$scope.answers[i].QuestionID - 1].Answers.
+                        push({ ID: $scope.answers[i].ID, Text: $scope.answers[i].Text });
+                }
+            });
         });
     });
 
-    $scope.save = function (user, question, answer) {
+    $scope.save = function () {
         $http({
             method: 'POST',
             url: url + '/api/UserAnswers',
@@ -43,19 +46,8 @@
         });
     };
 
-    $scope.next = function () {
-        for (var i = 0; i < $scope.questions[$scope.currentIndex - 1].Answers.length; i++) {
-            if ($scope.userAnswers[$scope.questions[$scope.currentIndex - 1].Answers[i].ID] == true) {
-                // TODO: get real user id
-                $scope.save(1, $scope.currentIndex - 1, $scope.questions[$scope.currentIndex - 1].Answers[i].ID);
-            }
-        } 
-
+    $scope.next = function (questionID) {
+      //  $scope.toSend.push({ question: questionID, answer:  })
         $scope.currentIndex++;
     };
-
-    $scope.end = function () {
-        $scope.next();
-        window.location.href = url + '/DJs/Index'
-    }
 });
