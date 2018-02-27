089eb10320ce3101109f3b4789c7fc740ec09ba3
michaltev <michaltev@gmail.com>
2018-02-27 20:43:06 +0200

    טבלת האירועים עובדת עבור יוזר שמספרו 1

--- DJMatch/Scripts/Events/usersEventsController.js
+++ DJMatch/Scripts/Events/usersEventsController.js
@@ -1,18 +1,49 @@
 ﻿djApp.controller("usersEventsController", function djsController($scope, $q, $http) {
 
     if (($scope.eventsList == undefined) || ($scope.eventsList, length == 0)) {
+        $scope.eventsList = [];
+
         var promises = [];
 
         var defer = $q.defer();
 
         promises.push($http({
             method: 'GET',
-            url: url + '/api/Users/' + userID + '/events'
+            url: url + '/api/Users/' + '1'/*userID*/ + '/events'
         }));
 
         //Resolve all promise into the promises array
         $q.all(promises).then(function (response) {
-            $scope.eventsList = response[0].data;
+            var eventsList = response[0].data;
+
+            for (var i = 0; i < eventsList.length; i++) {
+                debugger;
+                var currEvent = eventsList[i];
+
+                var innerPromises = [];
+
+                var innerDefer = $q.defer();
+
+                innerPromises.push($http({
+                    method: 'GET',
+                    url: url + '/api/playlists/' + currEvent.PlaylistId
+                }));
+
+                innerPromises.push($http({
+                    method: 'GET',
+                    url: url + '/api/DJs/' + currEvent.DJId
+                }));
+
+                //Resolve all promise into the promises array
+                $q.all(innerPromises).then(function(response) {
+                    currEvent.playlistName = response[0].data.Name;
+                    currEvent.DJName = response[1].data.Name;
+
+                    $scope.eventsList.push(currEvent);
+                });
+                
+            }
+
         });
     }
 
