760570b69b9ef455ed01050d4a01755bf2678dd5
שיר זרחיה < @LAPTOP-CTMBKJS4>
2018-04-09 19:12:58 +0300

    הוספת עמוד אירוע נוכחי

---
+++ DJMatch/Scripts/Events/eventDetails.js
@@ -0,0 +1,11 @@
+﻿djApp.controller('eventDetailsController',
+    function eventDetailsControoler($scope, $http) {
+        $scope.eventId = (new URL(window.location.href )).searchParams.get("id");
+
+        $http({
+            method: 'GET',
+            url: url + '/api/Events/' + $scope.eventId
+        }).then(function successCallback(response) {
+            $scope.currEvent = response.data;
+        });
+    });
