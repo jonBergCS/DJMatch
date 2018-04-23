89dd5ba0957703cbc2d61069fc0d4afb357ca2aa
jonBergCS <johnathanberg@cs.colman.ac.il>
2018-04-23 14:08:11 +0300

    Temp bandaid for learning machine

--- DJMatch/MachineLearning/LearningEngine.cs
+++ DJMatch/MachineLearning/LearningEngine.cs
@@ -37,21 +37,38 @@ namespace DJMatch
 
             //this.dtree = new DecisionTree(dvar, 100);
 
-            
-            int[][] inputs =
-                (from user in db.Users
-                 where user.Events.Count > 0
-                 select new int[] {
-                     user.UserAnswers.FirstOrDefault(ans => ans.QuestionID == 1).AnswerID,
-                     user.UserAnswers.FirstOrDefault(ans => ans.QuestionID == 4).AnswerID,
-#pragma warning disable CS0618 // Type or member is obsolete
-                     code.Translate(string.Join("; ", user.UserAnswers.Where(ans => ans.QuestionID == 2)))[0],
-#pragma warning restore CS0618 // Type or member is obsolete
-                     user.UserAnswers.FirstOrDefault(ans => ans.QuestionID == 3).AnswerID,
-                     user.UserAnswers.FirstOrDefault(ans => ans.QuestionID == 6).AnswerID,
-                     user.ID
-                 }).ToArray();
+            //int[][] inputs =
+            //    (from user in db.Users
+            //     where user.Events.Count > 0
+            //     select new int[] {
+            //         user.UserAnswers.FirstOrDefault(ans => ans.QuestionID == 1).AnswerID,
+            //         user.UserAnswers.FirstOrDefault(ans => ans.QuestionID == 4).AnswerID,
+            //         user.UserAnswers.Where(ans => ans.QuestionID == 2)
+            //            .Average(an => an.Answer.Text.Average(s=> s)),
+            //         user.UserAnswers.FirstOrDefault(ans => ans.QuestionID == 3).AnswerID,
+            //         user.UserAnswers.FirstOrDefault(ans => ans.QuestionID == 6).AnswerID,
+            //         user.ID
+            //     }).ToArray();
 
+            int[][] inputs = db.Users.Where(user => user.Events.Count > 0)
+                .Select(user => new
+                {
+                    a = (int?) user.UserAnswers.FirstOrDefault(ans => ans.QuestionID == 1).AnswerID ?? 0,
+                    b = (int?)user.UserAnswers.FirstOrDefault(ans => ans.QuestionID == 4).AnswerID ?? 0,
+                    c = user.UserAnswers.Where(ans => ans.QuestionID == 2).Select(ans => ans.Answer.Text),
+                    d = (int?)user.UserAnswers.FirstOrDefault(ans => ans.QuestionID == 3).AnswerID ?? 0,
+                    e = (int?)user.UserAnswers.FirstOrDefault(ans => ans.QuestionID == 6).AnswerID ?? 0,
+                    f = (int?)user.ID ?? 0
+                }).AsEnumerable()
+                .Select(user => new int[] 
+                              { user.a,
+                                user.b,
+                                code.Translate(string.Join(";", user.c))[0],
+                                user.d,
+                                user.e,
+                                user.f })
+                .ToArray();
+           
             List<int> outputList = new List<int>();
 
             foreach (int[] currVect in inputs)
@@ -77,7 +94,9 @@ namespace DJMatch
         {
             try
             {
-                return db.DJs.Find(this.dtree.Decide(this.UserToVector(userID)));
+                int chosenOne = this.dtree.Decide(this.UserToVector(userID));
+
+                return db.DJs.Find(chosenOne);
             }
             catch (Exception)
             {
