d18e479680ef543d88dcca4d9e216449d373733e
michaltev <michaltev@gmail.com>
2018-05-27 22:22:18 +0300

    לא מצליח לשמור את הדיג'י על הקוקיז משהו הזוי, אז כרגע זה הארד קודד אבל חוץ מזה השמירה של איוונט חדש מעיפהשגיאה בגלל הההמרה מג'ייסון לאובייקט

--- DJMatch/Scripts/Events/createEventController.js
+++ DJMatch/Scripts/Events/createEventController.js
@@ -1,11 +1,57 @@
-﻿djApp.controller("createEventController", function createEventController($scope) {
-    //var url = new URL(window.location.href);
-    $scope.currUser = "Michal Teverovsky";
-    $scope.DJ = { Name: "Skazi" };
+﻿djApp.controller("createEventController", function createEventController($scope, $q, $http, generalFactory) {
+
+    var promises = [];
+    var userID = generalFactory.getCookieData();
+    var DJID = generalFactory.getDJData();
+    $scope.currEvent = {"DJId":DJID, "UserId":userID};
+    
+    promises.push($http({
+        method: 'GET',
+        url: url + '/api/DJs/' + 1 //TODO!!!!
+    }));
+
+    promises.push($http({
+        method: 'GET',
+        url: url + '/api/Users/' + userID
+    }));
+
+    promises.push($http({
+        method: 'GET',
+        url: url + '/api/UserAnswers/eventData/' + userID
+    }));
+
+    //Resolve all promise into the promises array
+    $q.all(promises).then(function (response) {
+        $scope.currDJ = response[0].data;
+        $scope.currUser = response[1].data;
+        debugger;
+        var answers = response[2].data;
+        $scope.currEvent.Date = answers.date;
+        $scope.currEvent.EventType = answers.type;
+        
+    });
+
+
     $scope.currEvent = { Date: "23/03/2019", EventType: "wedding" };
-    $scope.currPlaylist = { Name: "Wedding" };
 
     $scope.createEvent = function () {
-        window.location.href = '/Events/Details?id=1';
+        $http({
+            method: "POST",
+            url: '/api/Events',
+            data: $scope.currEvent
+
+        }).then(function (d) {
+            if (d.data != null) {
+                generalFactory.setEventData(d.data.ID);
+                window.location.href = '/Events/Details?id=' + d.data.ID;
+            } else {
+                alert("The event has a problem");
+            }
+        },
+            function () {
+                alert('failed');
+            });
+
+        generalFactory.clearDJData();
     };
 });
--- DJMatch/Scripts/Home/generalService.js
+++ DJMatch/Scripts/Home/generalService.js
@@ -1,9 +1,11 @@
 ﻿djApp.factory("generalFactory", function ($cookies) {
     var userID = "";
+    var eventID = "";
+    var DJID = "";
 
     return {
-        setCookieData: function (userID) {
-            userID = userID;
+        setCookieData: function (_userID) {
+            userID = _userID;
             $cookies.put("userID", userID);
         },
         getCookieData: function () {
@@ -15,8 +17,8 @@
             $cookies.remove("userID");
         },
 
-        setEventData: function (eventID) {
-            eventID = eventID;
+        setEventData: function (_eventID) {
+            eventID = _eventID;
             $cookies.put("eventID", eventID);
         },
         getEventData: function () {
@@ -26,6 +28,19 @@
         clearEventData: function () {
             eventID = "";
             $cookies.remove("eventID");
+        },
+
+        setDJData: function (_DJID) {
+            DJID = _DJID;
+            $cookies.put("djID", DJID);
+        },
+        getDJData: function () {
+            DJID = $cookies.get("djID");
+            return DJID;
+        },
+        clearDJData: function () {
+            DJID = "";
+            $cookies.remove("djID");
         }
     };
 });
